4a0a8fb8d8d4849672a649680374252578bdcd72
chykB <malikchika86@gmail.com>
2024-02-05 19:30:04 +0100

    A calculator program

---
+++ main.py
@@ -0,0 +1,27 @@
+""" 
+    User input for 2 number and operators
+"""
+a = int(input('Enter a number: '))
+b = int(input('Enter another number: '))
+operator = input('Select: + for addition, - for subtraction, * for multiplication, / for division: ')
+
+"""
+Perform operation on numbers and display the result
+"""
+if operator == '+':
+    result = a + b
+    print(f"{result}")
+elif operator == '-':
+    result = a - b
+    print(f"{result}")
+elif operator == '*':
+    result = a * b
+    print("{result}")
+elif operator == '/':
+    if b == 0:
+        print(" Please select numbers other than zero ")
+    else:
+        result = a / b
+        print(f"{result}")
+else:
+    print("Invalid! Please select a valid operator")

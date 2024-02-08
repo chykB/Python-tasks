1144c125986da769c0a7219574e5f0987c1c8bbd
chykB <malikchika86@gmail.com>
2024-02-08 15:18:45 +0100

    Task 2

--- fast-api/routers/fullname_router.py
+++ fast-api/routers/fullname_router.py
@@ -5,13 +5,13 @@ router = APIRouter()
 users = []
 
 # Create a new user
-@router.post("/users/", response_model=User)
+@router.post("/users/")
 async def create_user(user: User):
     users.append(user) 
     return user
 
 # Get the full name of a user from their first and last names
-@router.get("/user", response_model=str)
+@router.get("/user")
 async def get_user(user: User):
     full_name = f"{user.first_name} {user.last_name}"
     return full_name

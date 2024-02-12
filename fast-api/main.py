7e52510c7d51cf36d92c6b2fd8a8d7259bf33e26
chykB <malikchika86@gmail.com>
2024-02-12 23:15:17 +0100

    fix: updated the endpoint

--- fast-api/main.py
+++ fast-api/main.py
@@ -1,10 +1,11 @@
 # malikchika86@gmail.com
 from fastapi import FastAPI
-from routers import fullname_router
+from routers import fullname_router, getpdf
 
 app = FastAPI()
 
 app.include_router(fullname_router.router)
+app.include_router(getpdf.router)
 
 if __name__ == "__main__":
     import uvicorn
---
+++ fast-api/routers/getpdf.py
@@ -0,0 +1,31 @@
+from fastapi import File, UploadFile, APIRouter
+import os
+
+router = APIRouter()
+
+@router.post("/upload_pdf")
+async def upload_pdf(file: UploadFile = File(...), directory_path=None):
+    """
+        This endpoint collect a PDF file and saves it to the specified directory path. 
+        Returns a dictionary indicating the success status and a message.
+        malikchika86@gmail.com
+    """
+    try:
+        filename, file_extension = os.path.splitext(file.filename)
+        if file_extension.lower() != ".pdf":
+            raise ValueError("Upload a pdf file")
+        
+        if directory_path:
+            upload_directory = os.path.abspath(directory_path)
+            os.makedirs(upload_directory, exist_ok=True)
+        else: 
+            upload_directory = os.path.abspath(".")
+
+        file_path = os.path.join(upload_directory, file.filename)
+        
+        with open(file_path, "wb") as f:
+            f.write(file.file.read())
+
+        return {"success": "File uploaded successfully"}
+    except Exception as e:
+        return {"error": str(e)}
